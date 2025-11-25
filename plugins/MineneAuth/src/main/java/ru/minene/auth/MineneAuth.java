b267cd79d95c29947ba0583f195ae1d931e64c65
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2025-11-25 21:29:18 +0600

    Update MineneAuth.java

--- plugins/MineneAuth/src/main/java/ru/minene/auth/MineneAuth.java
+++ plugins/MineneAuth/src/main/java/ru/minene/auth/MineneAuth.java
@@ -1,6 +1,7 @@
 package ru.minene.auth;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
